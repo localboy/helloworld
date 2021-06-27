ccc50ac09989d26fceaa11db047e0c98dd99f001
Mohammad Jakir Hossain <mjakir.ddd@gmail.com>
2021-06-27 11:45:41 +0600

    Complete Todo list

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,95 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import {
+    addTodos,
+    completeTodos,
+    removeTodos,
+    updateTodos,
+} from "../redux/reducer";
+import TodoItem from "./todo/TodoItem";
+import Todos from "./todo/Todos";
 
-const TodoList = () => {
+
+const mapStateToProps = (state) => {
+    return {
+        todos: state,
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        addTodo: (obj) => dispatch(addTodos(obj)),
+        removeTodo: (id) => dispatch(removeTodos(id)),
+        updateTodo: (obj) => dispatch(updateTodos(obj)),
+        completeTodo: (id) => dispatch(completeTodos(id)),
+    };
+};
+
+const TodoList = (props) => {
+    const [sort, setSort] = useState("active");
     return (
-        <div>
-            <h5>Welcome to Todo List</h5>
-            <Link to='/'>return</Link>
+        <div className="row justify-content-center pt-3">
+            <div className="col-md-8 col-sm-12">
+                <Todos />
+                <div class="btn-group" role="group" aria-label="Basic example">
+                    <button type="button" class="btn btn-primary" onClick={() => setSort("active")}>Active</button>
+                    <button type="button" class="btn btn-primary" onClick={() => setSort("completed")}>Completed</button>
+                    <button type="button" class="btn btn-primary" onClick={() => setSort("all")}>All</button>
+                </div>
+                <div className="displaytodos">
+
+                    <ul className="list-group mt-2">
+                        {props.todos.length > 0 && sort === "active"
+                            ? props.todos.map((item) => {
+                                return (
+                                    item.completed === false && (
+                                        <TodoItem
+                                            key={item.id}
+                                            item={item}
+                                            removeTodo={props.removeTodo}
+                                            updateTodo={props.updateTodo}
+                                            completeTodo={props.completeTodo}
+                                        />
+
+                                    )
+                                );
+                            })
+                            : null}
+                        {/* for completed items */}
+                        {props.todos.length > 0 && sort === "completed"
+                            ? props.todos.map((item) => {
+                                return (
+                                    item.completed === true && (
+                                        <TodoItem
+                                            key={item.id}
+                                            item={item}
+                                            removeTodo={props.removeTodo}
+                                            updateTodo={props.updateTodo}
+                                            completeTodo={props.completeTodo}
+                                        />
+                                    )
+                                );
+                            })
+                            : null}
+                        {/* for all items */}
+                        {props.todos.length > 0 && sort === "all"
+                            ? props.todos.map((item) => {
+                                return (
+                                    <TodoItem
+                                        key={item.id}
+                                        item={item}
+                                        removeTodo={props.removeTodo}
+                                        updateTodo={props.updateTodo}
+                                        completeTodo={props.completeTodo}
+                                    />
+                                );
+                            })
+                            : null}
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
 
-export default TodoList;
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
